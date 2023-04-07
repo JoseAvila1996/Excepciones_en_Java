6ee689c2f42fd0751752a5e6ea865ce2f8fefbdd
Avila <avila1996jose@gmail.com>
2023-04-06 19:56:48 -0500

    agregando y aplicando myexception ademas de comentarios explicativos

--- Java-pila-ejecucion/src/Flujo.java
+++ Java-pila-ejecucion/src/Flujo.java
@@ -7,12 +7,19 @@ public class Flujo {
 	}
 	public static void metodo1() {
 		System.out.println("Inicio metodo1");
-		metodo2();
+		try {
+			metodo2();
+			
+		} catch (MyException me) {
+			// TODO: handle exception
+			me.printStackTrace();
+		}
+		
 		System.out.println("Fin metodo1");
 	}
 	private static void metodo2() {
 		System.out.println("inicio metodo 2");
-		throw new ArithmeticException("surgio error");
+		throw new MyException("surgio error");
 	
 	}
 }
